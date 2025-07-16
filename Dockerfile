f112f95525d17d94dd2671c4145460e9f2e6ea6d
Gennady Oleshko <gennady@oleshko-g.ru>
2025-07-16 08:54:58 +0300

    set up PORT env variable

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,6 @@ FROM debian:stable-slim
 
 COPY learn-docker /bin/learn-docker
 
+ENV PORT="8991"
+
 CMD [ "/bin/learn-docker" ]
--- main.go
+++ main.go
@@ -4,15 +4,14 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 )
 
 func main() {
 	m := http.NewServeMux()
-
 	m.HandleFunc("/", handlePage)
-
-	const port = "8010"
+	port := os.Getenv("PORT")
 	srv := http.Server{
 		Handler:      m,
 		Addr:         ":" + port,
